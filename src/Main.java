8917fc2612a809f7ff64867dcb2175ce2c1a0092
Prabesh Niroula <niroulaprabesh414@gmail.com>
2025-02-16 22:41:46 +1100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        //This is how you write a comment.
+        /*
+        multi-line comments
+         */
+        System.out.println("This is my first lesson.");
+        System.out.print("These are the basics.");
+        //if you type sout and hit tab it gives the command for print statement
+
+    }
+}
