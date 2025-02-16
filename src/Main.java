a41f6a7a020119f1079341a4a353c88ff96b56b5
Prabesh Niroula <niroulaprabesh414@gmail.com>
2025-02-16 23:08:19 +1100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args){
+    /*
+    Data types:
+    Primitive: simple value stored directly in memory (stack)
+    Reference: memory address (stack) that points to the (heap)
 
-        //This is how you write a comment.
-        /*
-        multi-line comments
-         */
-        System.out.println("This is my first lesson.");
-        System.out.print("These are the basics.");
-        //if you type sout and hit tab it gives the command for print statement
+     */
+        int age = 30;
+        double temperature = -23.5;
+        char Currency = '$';
+        String Name = "Prabesh Niroula";
+
+        System.out.println("My name is "+ Name + ". "+ "My age is :"+ age + "."+ "The currency I use is :"+ Currency + "."+ "My temperature is: "+ temperature+".");
+
 
     }
 }
