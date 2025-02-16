412d1ee642c9345313ffed4640bae6e4c85eecd4
Prabesh Niroula <niroulaprabesh414@gmail.com>
2025-02-16 23:29:02 +1100

    third commit

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,17 @@ public class Main {
         int age = 30;
         double temperature = -23.5;
         char Currency = '$';
+        Boolean isMale = true;
         String Name = "Prabesh Niroula";
 
-        System.out.println("My name is "+ Name + ". "+ "My age is :"+ age + "."+ "The currency I use is :"+ Currency + "."+ "My temperature is: "+ temperature+".");
-
+        //System.out.println("My name is "+ Name + ". "+ "My age is :"+ age + "."+ "The currency I use is :"+ Currency + "."+ "My temperature is: "+ temperature+".");
+        if (isMale){
+            System.out.println("You are a grizzly man.");
+        }
+        else {
+            System.out.println("You are a female.");
+        }
 
     }
+
 }
